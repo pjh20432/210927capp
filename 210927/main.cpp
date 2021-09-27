d83b3ad59a4740f3b8344bfcd85bd866e9b077fc
pjh <pjh20432@naver.com>
2021-09-27 10:38:49 +0900

    main.cpp PintInt() 함수완료

--- 210927/main.cpp
+++ 210927/main.cpp
@@ -1,6 +1,31 @@
 #include<stdio.h>
 
+void PrintInt(int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("Hello : %d\n", i);
+}
+
+
+
 int main()
 {
-	printf("hello");
+	PrintfInt(100;);
 }
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	for (int i = 0; i < 10; i++)
+//		printf("Hello : %d\n", i);
+//}
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("hello");
+//}
